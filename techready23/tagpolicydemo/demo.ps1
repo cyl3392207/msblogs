ec8600f66a0ec7b814743c6858d7dfa4b894dd0b
Mike Chen <mikech@microsoft.com>
2017-04-04 12:36:38 -0700

    Convert to parameterized policy

--- techready23/tagpolicydemo/demo.ps1
+++ techready23/tagpolicydemo/demo.ps1
@@ -1,104 +1,40 @@
 Login-AzureRmAccount
 
-$group = Get-AzureRmResourceGroup -Name "Group" 
-
-$resources = Find-AzureRmResource -ResourceGroupName $group.ResourceGroupName 
+$group = New-AzureRmResourceGroup -Name "TestGroup" -Location westus
 
 
-$tagPolicy = @()
-$tagPolicy+=New-AzureRmPolicyDefinition -Name appendcostCenternotag -DisplayName "Append Cost Center Tag (no tag)" -Policy '{
+$tagPolicy = New-AzureRmPolicyDefinition -Name appendtagpolicyv2 -DisplayName "Append Tag and its Value"  -Policy '{
   "if": {
-    "field": "tags",
+    "field": "[parameters(''tagName'')]",
     "exists": "false"
   },
   "then": {
     "effect": "append",
     "details": [
       {
-        "field": "tags",
-        "value": {
-          "costCenter": "myDepartment"
-        }
+        "field": "[parameters(''tagName'')]",
+        "value": "[parameters(''tagValue'')]"
       }
     ]
   }
-}'
-
-$tagPolicy+= New-AzureRmPolicyDefinition -Name appendcostCenterothertag -DisplayName "Append Cost Center Tag (other tag)" -Policy '{
-  "if": {
-    "allOf": [
-      {
-        "field": "tags",
-        "exists": "true"
-      },
-      {
-        "field": "tags.costCenter",
-        "exists": "false"
-      }
-    ]
-  },
-  "then": {
-    "effect": "append",
-    "details": [
-      {
-        "field": "tags.costCenter",
-        "value": "myDepartment"
-      }
-    ]
-  }
-}'
-
-$tagPolicy+= New-AzureRmPolicyDefinition -Name denyifnocostCenter -DisplayName "Block updates for cost center tag" -Policy '
-{
-  "if": {
-    "not": {
-      "field": "tags.costCenter",
-      "equals": "myDepartment"
+}' -Parameter '{
+  "tagValue": {
+    "type": "string",
+    "metadata": {
+      "description": "value of Tag"
     }
   },
-  "then": {
-    "effect": "deny"
+  "tagName": {
+    "type": "string",
+    "metadata": {
+      "description": "Name of Tag"
+    }
   }
 }'
 
+New-AzureRmPolicyAssignment -Name "appendcostcenter" -PolicyDefinition $tagPolicy -Scope $group.Resourceid -tagName "tags.costCenter" -tagValue "00001"
 
-New-AzureRmPolicyAssignment -Name "appendcostcenternotags" -PolicyDefinition $tagPolicy[0] -Scope $group.Resourceid
-New-AzureRmPolicyAssignment -Name "appendcostcenternoothertag" -PolicyDefinition $tagPolicy[1] -Scope $group.Resourceid
-New-AzureRmPolicyAssignment -Name "denycostcentertagupdate" -PolicyDefinition $tagPolicy[2] -Scope $group.Resourceid
+$yourowntag = @{ Environment="Test" } 
 
+New-AzureRMStorageAccount -Name "fdsacdsa322fdasfdsa3cdxf" -ResourceGroupName $group.ResourceGroupName -SkuName Standard_LRS -Location westus -Tag $yourowntag
 
-# relogin to refresh the token, so that policy cache are refreshed. There is a up to 30 minutes delay since policy cache will be refrehsed every 30 minutes. Relogin can refrehh the cache
-Login-AzureRmAccount
-
-# Verify cost center tags are appended, even the request doesn't contains cost center tag
-New-AzureRmStorageAccount -ResourceGroupName $group.ResourceGroupName -Name ("tsaccc" + (Get-Random -Minimum 1 -Maximum 1000)) -SkuName Standard_LRS -Location westus -Kind Storage
-
-
-# applying a patch for existing resources  
-foreach($r in $resources)
-{
-     try{
-         $r | Set-AzureRmResource -Tags ($a=if($_.Tags -eq $NULL) { @{}} else {$_.Tags}) -Force -UsePatchSemantics
-     }
-     catch{
-         Write-Host  $r.ResourceId + "can't be updated"
-     }
-}
-
-# clean up after the demo
-
-Get-AzureRMPolicyAssignment -Scope $Group.ResourceId | Remove-AzureRmPolicyAssignment -Scope $group.ResourceId
-
-# relogin to refresh the token, so that policy cache are refreshed.
-Login-AzureRmAccount
-
-
-foreach($r in $resources)
-{
-     try{
-         $r | Set-AzureRmResource -Tag @{} -Force 
-     }
-     catch{
-         Write-Host  $r.ResourceId + "can't be updated"
-     }
-}
